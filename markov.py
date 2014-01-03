5715a1fb2a183e40fb342468a726dbdcb00c3828
Steven T <steven.nk.tran@gmail.com>
2014-01-03 11:26:52 -0700

    added rhythms

--- markov.py
+++ markov.py
@@ -2,73 +2,85 @@ from __future__ import division
 from music21 import *
 from scipy import stats
 import numpy as np
-import pprint
 
 def pairwise2(iterable):
-  return zip(iterable, iterable[1:]) + [(iterable[-1], note.Rest())]
+  return zip(iterable, iterable[1:]) + [(iterable[-1], iterable[0])]
 
 if __name__ == '__main__':
 
 	sc = scale.MajorScale('c')
 
 	# C, D, E, F, G, A, B, rest 
-	Matrix = [[0 for x in xrange(8)] for x in xrange(8)]
+	PitchMatrix = [[.0000001 for x in xrange(8)] for x in xrange(8)]
 
-	mozart = converter.parseFile('1_1.krn')
-	melody = mozart.parts[0]
-	for measure in melody.getElementsByClass('Measure'):
-		for pair in pairwise2(measure.notesAndRests):
-			if pair[0].isChord:
-			 	topNote = note.Note(pair[0].pitches[len(pair[0]) - 1])
-				firstdegree = sc.getScaleDegreeFromPitch(topNote)
-			elif pair[0].isRest:
-				firstdegree = 8
-			else:
-				firstdegree = sc.getScaleDegreeFromPitch(pair[0])
-			#print firstdegree
+	# quarter lenghts
+	# 0, .25, .5, .75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4
+	DurationMatrix = [[.0000001 for x in xrange(17)] for x in xrange(17)]
 
-			if pair[1].isChord:
-			 	topNote = note.Note(pair[1].pitches[len(pair[1]) - 1])
-				seconddegree = sc.getScaleDegreeFromPitch(topNote)
-			elif pair[1].isRest:
-				seconddegree = 8
-			else:
-				seconddegree = sc.getScaleDegreeFromPitch(pair[1])
-			#print seconddegree
+	mozart = converter.parseFile('C:\Users\Steven\Documents\Twinkle.xml')
+	melody = mozart[2].flat
+	#melody = mozart.parts[0].flat
+	for pair in pairwise2(melody.notesAndRests):
+		firstDuration = int(pair[0].quarterLength * 4)
+		if pair[0].isChord:
+			topNote = note.Note(pair[0].pitches[len(pair[0]) - 1])
+			firstdegree = sc.getScaleDegreeFromPitch(topNote)
+		elif pair[0].isRest:
+			firstdegree = 8
+		else:
+			firstdegree = sc.getScaleDegreeFromPitch(pair[0])
 
-			if firstdegree and seconddegree:
-				Matrix[firstdegree - 1][seconddegree - 1] = Matrix[firstdegree - 1][seconddegree - 1] + 1
-	#pprint.pprint(Matrix)
+		secondDuration = int(pair[1].quarterLength * 4)
+		if pair[1].isChord:
+			topNote = note.Note(pair[1].pitches[len(pair[1]) - 1])
+			seconddegree = sc.getScaleDegreeFromPitch(topNote)
+		elif pair[1].isRest:
+			seconddegree = 8
+		else:
+			seconddegree = sc.getScaleDegreeFromPitch(pair[1])
+
+		if firstdegree and seconddegree:
+			PitchMatrix[firstdegree - 1][seconddegree - 1] = PitchMatrix[firstdegree - 1][seconddegree - 1] + 1
+			DurationMatrix[firstDuration][secondDuration] = DurationMatrix[firstDuration][secondDuration] + 1
 
 	#normalize
-	for row in Matrix:
+	for row in PitchMatrix:
 		total = sum(row)
 		for i in xrange(len(row)):
    			row[i] = row[i]/float(total)
-	#pprint.pprint(Matrix)
-
+	for row in DurationMatrix:
+		total2 = sum(row)
+		for i in xrange(len(row)):
+			row[i] = row[i]/float(total2)
 
 	xk = np.arange(8)
+	xk2 = np.arange(17)
 
-	custm = [0 for x in xrange(8)]
+	distPitch = [0 for x in xrange(8)]
 	for x in xrange(8):
-		custm[x] = stats.rv_discrete(name='custm', values=(xk, Matrix[x]))
+		distPitch[x] = stats.rv_discrete(name='distPitch', values=(xk, PitchMatrix[x]))
+
+	distDuration = [0 for x in xrange(17)]
+	for x in xrange(17):
+		distDuration[x] = stats.rv_discrete(name='distDuration', values=(xk2, DurationMatrix[x]))
 
 	compose = stream.Stream()
-	n1 = note.Note('c5')
+	n1 = note.Note('c4')
 	compose.append(n1)
 
-	for x in range(0, 100):
+	for x in range(0, 40):
+		indexDuration = int(n1.quarterLength * 4)
 		if n1.isRest:
 			index = 7
 		else:
 			index = sc.getScaleDegreeFromPitch(n1.pitch)
-		R = custm[index - 1].rvs(size=1) + 1
+		R = distPitch[index - 1].rvs(size=1) + 1
+		R2 = int(distDuration[indexDuration].rvs(size=1))
 		pitch = sc.pitchesFromScaleDegrees(R)[0]
 		if R == 8:
-			n2 = note.Rest(quarterLength = 1)
+			n2 = note.Rest(quarterLength = R2/4.0)
 		else:
-			n2 = note.Note(pitch, quarterLength = .5)
+			n2 = note.Note(pitch, quarterLength = (R2/4.0))
 		compose.append(n2)
 		n1 = n2
 
